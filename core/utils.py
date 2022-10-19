72680e69e261939a17041e94bb0cc23d0affa4ae
izzat1998 <izzattilla706@gmail.com>
2022-10-19 13:22:54 +0500

    for station adding

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,6 @@
 import xlrd
 
-from core.models import Station
+from core.models import Station, Product
 
 
 def add_stations():
@@ -13,3 +13,15 @@ def add_stations():
                 code=str(worksheet.cell(i, 3).value).replace(".0", ''),
                 railway_name=worksheet.cell(i, 2).value
             )
+
+
+def add_products():
+    workbook = xlrd.open_workbook("gng.xls")
+    worksheet = workbook.sheet_by_index(0)
+    for i in range(1, 17616):
+        Product.objects.get_or_create(
+            name=worksheet.cell(i, 2).value,
+            hc_code=str(worksheet.cell(i, 1).value).replace(".0", ''),
+            etcng_code=str(worksheet.cell(i, 3).value).replace(".0", ''),
+            etcng_name=worksheet.cell(i, 4).value,
+        )
--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from rest_framework.filters import SearchFilter
 
 from core.models import Product, Station
 from core.serializers import ProductSerializer, StationSerializer
+from core.utils import add_products
 
 
 # Create your views here.
@@ -10,7 +11,7 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter]
     search_fields = ['name', 'hc_code', 'etcng_code', 'etcng_name']
-    queryset = Product.objects.all()
+    queryset = add_products()
 
 
 class StationViewSet(viewsets.ModelViewSet):
