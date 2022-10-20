c17fabef9ad5d26fe30e27c188c7c73ddaf28e7c
izzat1998 <izzattilla706@gmail.com>
2022-10-20 12:02:49 +0500

    added deploy function

--- order/urls.py
+++ order/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('list/', ContainerOrderList.as_view(), name='container_order_list'),
     path('list/<int:order_number>/', ContainerOrderDetail.as_view(), name='container_order_detail'),
     path('create/', ContainerOrderCreate.as_view(), name='container_order_create'),
+    path('create2/', ContainerOrderCreate.as_view(), name='container_order_create2'),
 ]
