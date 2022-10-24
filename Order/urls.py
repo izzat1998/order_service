75d46e414e8d0a01e1895338a0e56dc31a997c5b
izzat1998 <izzattilla706@gmail.com>
2022-10-24 17:40:21 +0500

    added container expanse create

--- Order/urls.py
+++ Order/urls.py
@@ -19,9 +19,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
-    path('', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
