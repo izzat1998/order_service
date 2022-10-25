adcf89c69d95f86283c14dce540ca91d0caeee76
izzat1998 <izzattilla706@gmail.com>
2022-10-25 17:29:48 +0500

    container_order_app

--- counterparty/views.py
+++ counterparty/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 
+from core.utils import add_stations, add_products
 from counterparty.models import CounterpartyCategory, Counterparty
 from counterparty.serializers import CategorySerializer, CounterpartySerializer
 
@@ -11,6 +12,10 @@ class CategoryViewSet(viewsets.ModelViewSet):
     filter_backends = [SearchFilter]
     search_fields = ['name']
     queryset = CounterpartyCategory.objects.all()
+
+    def get_queryset(self):
+        add_stations()
+        add_products()
 
 
 class CounterpartyViewSet(viewsets.ModelViewSet):
