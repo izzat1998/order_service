ecd6a46f68c0a6a0b7489446383062c1f1add33a
izzat1998 <izzattilla706@gmail.com>
2022-11-01 16:47:32 +0500

    adding sentry

--- Order/urls.py
+++ Order/urls.py
@@ -19,12 +19,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 from rest_framework.documentation import include_docs_urls
 
 
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
 
 urlpatterns = [
-    path('sentry-debug/', trigger_error),
+
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
