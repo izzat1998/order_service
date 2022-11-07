bb7d58406399bca8a78dc27ef20dfd600d993702
izzat1998 <izzattilla706@gmail.com>
2022-11-07 16:27:21 +0500

    checking

--- core/apps.py
+++ core/apps.py
@@ -1,10 +1,7 @@
 from django.apps import AppConfig
 
 
-class WagonOrderConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-<<<<<<<< HEAD:apps/wagon_order/apps.py
-    name = 'apps.wagon_order'
-========
     name = 'core'
->>>>>>>> b987b94 (Revert "totally changed django app structure"):core/apps.py
+
