852455ecffabbd9be87fe3fb89c4124c4c79d433
izzat1998 <izzattilla706@gmail.com>
2022-11-08 16:17:00 +0500

    changed api structure

--- Order/settings.py
+++ Order/settings.py
@@ -67,9 +67,6 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 
-    'SWAGGER_UI_DIST': 'SIDECAR',  # shorthand to use the sidecar instead
-    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
-    'REDOC_DIST': 'SIDECAR',
     # OTHER SETTINGS
 }
 
