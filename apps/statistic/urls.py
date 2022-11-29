6123b58dcd0fee6afdb9dc6571d45d2f2874c8d2
izzat1998 <izzattilla706@gmail.com>
2022-11-29 18:26:02 +0500

    statistic update

--- apps/statistic/urls.py
+++ apps/statistic/urls.py
@@ -4,5 +4,5 @@ from apps.statistic.views import OrderStatistic, OrderStatisticMonthly
 
 urlpatterns = [
     path('', OrderStatistic.as_view(), name='order_statistic'),
-    path('/monthly', OrderStatisticMonthly.as_view(), name='order_statistic_monthly')
+    path('monthly', OrderStatisticMonthly.as_view(), name='order_statistic_monthly')
 ]
