3440a5d88bb4b751b5d70242ef6563175ac6769f
izzat1998 <izzattilla706@gmail.com>
2022-12-01 17:01:52 +0500

    empty wagon update

--- apps/wagon_empty_order/expanse/urls.py
+++ apps/wagon_empty_order/expanse/urls.py
@@ -4,6 +4,6 @@ from apps.wagon_empty_order.expanse.views import WagonEmptyExpanseUpdate, WagonE
 
 urlpatterns = [
 
-    # path('update/<int:pk>/', WagonEmptyExpanseUpdate.as_view(), name='update-empty-wagon'),
-    path('actual_cost/update/<int:pk>/', WagonEmptyActualCostUpdate.as_view(), name='update-empty-wagon')
+    path('update/<int:pk>/', WagonEmptyExpanseUpdate.as_view(), name='update-empty-wagon'),
+    # path('actual_cost/update/<int:pk>/', WagonEmptyActualCostUpdate.as_view(), name='update-empty-wagon')
 ]
