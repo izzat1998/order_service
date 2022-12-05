a00704ade99a343d0a03a894713194ddc986cf96
izzat1998 <izzattilla706@gmail.com>
2022-12-05 15:15:44 +0500

    optimized views order

--- apps/order/views.py
+++ apps/order/views.py
@@ -7,4 +7,4 @@ from .serializers.serializers import OrderSerializer
 
 class OrderList(ListAPIView):
     serializer_class = OrderSerializer
-    queryset = Order.objects.all()
+    queryset = Order.objects.all().select_related('departure', 'destination')
