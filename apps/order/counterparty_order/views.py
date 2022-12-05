66829ab55f8a231e8dc36869c7ec5f4815fdfacc
izzat1998 <izzattilla706@gmail.com>
2022-12-05 15:01:03 +0500

    deleted order_number from serializer

--- apps/order/counterparty_order/views.py
+++ apps/order/counterparty_order/views.py
@@ -15,20 +15,20 @@ from .serializers import (
 
 class CounterPartyOrderList(ListAPIView):
     serializer_class = CounterPartyOrderListSerializer
-    queryset = CounterPartyOrder.objects.all()
+    queryset = CounterPartyOrder.objects.all().select_related('order', 'counterparty', 'category')
 
 
 class CounterPartyOrderCreate(CreateAPIView):
     serializer_class = CounterPartyOrderCreateSerializer
-    queryset = CounterPartyOrder.objects.all()
+    queryset = CounterPartyOrder.objects.all().select_related('order', 'counterparty', 'category')
 
 
 class CounterPartyOrderUpdate(UpdateAPIView):
     lookup_field = "pk"
     serializer_class = CounterPartyUpdateSerializer
-    queryset = CounterPartyOrder.objects.all()
+    queryset = CounterPartyOrder.objects.all().select_related('order', 'counterparty', 'category')
 
 
 class CounterPartyOrderDelete(DestroyAPIView):
     lookup_field = "pk"
-    queryset = CounterPartyOrder.objects.all()
+    queryset = CounterPartyOrder.objects.all().select_related('order', 'counterparty', 'category')
