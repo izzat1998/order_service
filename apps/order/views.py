7d9b51daf170e1ffc8b5913690fba4667227d7ae
izzat1998 <izzattilla706@gmail.com>
2022-12-06 14:35:20 +0500

    order list filter

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,13 +1,18 @@
 # Create your views here.
-from rest_framework.filters import SearchFilter
-from rest_framework.generics import ListAPIView
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Order
 from .serializers.serializers import OrderSerializer
 
 
-class OrderList(ListAPIView):
-    serializer_class = OrderSerializer
-    queryset = Order.objects.all().select_related("departure", "destination")
-    filter_backends = [SearchFilter]
-    search_fields = ["manager"]
+class OrderList(APIView):
+    def get(self, request):
+        orders = Order.objects.all()
+        if 'manager' in request.GET:
+            orders = orders.filter(manager=request.GET['manager'])
+        if 'order_number' in request.GET:
+            orders = orders.filter(order_number=request.GET['order_number'])
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
