c8e7b3184579ff1adc74d1c59deda1d88564afec
izzat1998 <izzattilla706@gmail.com>
2022-12-15 15:57:41 +0500

    changed statistic

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -4,6 +4,6 @@ from .views import OrderList
 
 urlpatterns = [
     path("counterparty/", include("apps.order.counterparty_order.urls")),
-    path("list/", OrderList.as_view(), name="order-list"),
+    path("list/", OrderList.as_view(), name="order-list")
 
 ]
--- apps/order/views.py
+++ apps/order/views.py
@@ -5,12 +5,22 @@ from rest_framework.views import APIView
 
 from .models import Order
 from .serializers.serializers import OrderSerializer
+from ..core.models import Container
 
 
 class OrderList(APIView):
     def get(self, request):
 
         orders = Order.objects.filter(visible=True)
+        if 'container' in request.GET:
+            orders = orders.filter(container_order__container_types__expanses__container__name__icontains=request.GET[
+                'container'])
+        if 'manager' in request.GET:
+            orders = orders.filter(manager=request.GET['manager'])
+        if 'order_number' in request.GET:
+            orders = orders.filter(order_number=request.GET['order_number'])
+        if 'position' in request.GET:
+            orders = orders.filter(position=request.GET['position'])
         for order in orders:
             if hasattr(order, 'wagon_order'):
                 order.child_type = 'wagon_order'
@@ -18,11 +28,5 @@ class OrderList(APIView):
                 order.child_type = 'container_order'
             else:
                 order.child_type = 'wagon_empty_order'
-        if 'manager' in request.GET:
-            orders = orders.filter(manager=request.GET['manager'])
-        if 'order_number' in request.GET:
-            orders = orders.filter(order_number=request.GET['order_number'])
-        if 'position' in request.GET:
-            orders = orders.filter(position=request.GET['position'])
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
